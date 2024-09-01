25bd463690bd25aed4d20ae2c03ad4248182a5fb
yuankunzhang <fmcocoa@gmail.com>
2024-09-01 09:25:24 +0800

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+mod filters;
+
+use serde::Serializer;
+
+pub fn serde_query_option_vec<S, T, V>(v: V, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+    T: ToString,
+    V: AsRef<Option<Vec<T>>>,
+{
+    match v.as_ref() {
+        Some(v) => {
+            let arr: Vec<_> = v.iter().map(|x| x.to_string()).collect();
+            let str = format!("[{}]", arr.join(","));
+            s.serialize_str(&str)
+        }
+        None => s.serialize_none(),
+    }
+}
